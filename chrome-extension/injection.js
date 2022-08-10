1b7a7f8163e9733bee52b778d38a70a7db6b3522
Никита Киселев <nikitakiselev@Nikitas-MacBook-Air.local>
2022-08-10 20:15:53 +0500

    fix

--- chrome-extension/injection.js
+++ chrome-extension/injection.js
@@ -30,13 +30,19 @@ helper.addCombo("templates", {
     title: 'Templates',
     init: function() {
         "use strict";
-        
+
         this.startGroup("Topic templates");
         this.add("topicTemplate", "<h3>Topic</h3>", "Topic template");
         this.add("compTemplate", "<h3>Comprehension</h3>", "Comprehension template");
         this.add("appTemplate", "<h3>Application</h3>", "Application template");  
     },
     onClick: function (value) {
+        value = (value == "topicTemplate")
+            ? newTopicTemplate
+            : (value == "compTemplate")
+                ? newComprehensionTemplate
+                : newApplicationtemplate;
+        
         helper.editor.focus();
         helper.editor.fire('saveSnapshot');
         helper.editor.insertHtml(value);
@@ -47,3 +53,35 @@ helper.addCombo("templates", {
 helper.start();
 
 // helper.editor.getSelection().getRanges()[0].cloneContents().$
+
+const newTopicTemplate = `
+    <p>[PRE]</p>
+    <ul>
+        <li><a href="#" rel="noopener noreferrer nofollow">?</a></li>
+    </ul>
+    <p>Group: Golang -- ?</p>
+    <p>[/PRE]</p>
+    <p>
+        [META]Intro section[/META]
+    </p>
+    <h2>Section 1</h2>
+    <p>[META]Text[/META]</p>
+    <h2>Section 2</h2>
+    <p>[META]Text[/META]</p>
+    <h2>Section 3</h2>
+    <p>[META]Text[/META]</p>
+    <h2>Conclusion</h2>
+    <p>[META]Text[/META]</p>
+    <ul>
+        <li>...</li>
+    </ul>`;
+
+const newComprehensionTemplate = `
+    <p>[TITLE] ? # Comprehension [/TITLE]</p>
+    <p>[META]Text[/META]</p>
+    `;
+
+const newApplicationtemplate = `
+    <p>[TITLE] ? # Application [/TITLE]</p>
+    <p>[META]Text[/META]</p>
+    `;
